e58fb9baac2b4e84289049f1a1e1cbce5485d72d
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2022-11-10 16:16:27 +0100

    Solving AND problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 def main(npa):
     '''
     :param npa: numpy array representing an input boolean value in the function
@@ -21,6 +21,20 @@ def main(npa):
         print("Res6: ", res6)
         print("Result: ", bool(res1 or res2 or res3 or res4 or res5 or res6))
 
+def sigmoid(value):
+    print(1 / (1 + math.pow(math.e, -value)))
+    return round(1 / (1 + math.pow(math.e, -value)))
+
+
+def matrixes_product(x, w, b):
+    res_mat = np.empty(np.size(x, 0))
+    for i in range(np.size(x, 0)):
+        for j in range(np.size(w, 0)):
+            for k in range(np.size(b, 0)):
+                res = np.dot(x[i], w[j]) + b[k]
+                res_mat[i] = sigmoid(res)
+    print(res_mat)
+
 if __name__ == '__main__':
     npa = np.array([
         [False, False, False, False],
@@ -41,4 +55,46 @@ if __name__ == '__main__':
         [True, True, True, True]
         ])
 
-    main(npa)
+    '''
+        and_x = np.array([
+           # [0, 1, 0, 1], #Entrada de A
+            [0, 0, 1, 1] #Entrada de B
+        ])
+
+    [
+        [A1, B1],
+        [A2, B2],
+        [A3, B3],
+        [A4, B4]
+    ]
+    
+    [
+        [[2], [2]]
+    ]
+    
+    [
+        [b1],
+    ]
+    
+    '''
+
+    and_x = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1],
+        [1, 1]
+    ])
+
+    and_w1 = np.array([
+        [[2], [2]]
+    ])
+
+    and_bi = np.array([
+        [-1]
+    ])
+
+
+    #main(npa) #Solves the function
+    matrixes_product(and_x, and_w1, and_bi)
+
+
